ead23bf154f493e131b013f570c954c491d38886
Slava Pleshkov <slavapl22@gmail.com>
2024-06-20 14:00:57 +0500

    first commit

---
+++ src/main/java/com/pleshkov/conrollers/MyController.java
@@ -0,0 +1,17 @@
+package com.pleshkov.conrollers;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("api")
+public class MyController {
+
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "Hello and welcome!";
+    }
+}
